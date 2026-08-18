1953dec29ac483fa27202ce25d7394037a902a29
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-18 10:17:11 +0700

    membuat, menunjuk ke nama file tersebut, dan membuat inputnya

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -13,5 +13,12 @@ int main()
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
 
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
 }
