2af5542ba2cab004092fa7588339875c11ce3e30
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-18 10:43:51 +0700

    menambah program untuk menutup, membuka, dan menunjuk ke file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -29,5 +29,13 @@ int main()
         outfile << baris << endl;
 
     }
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
 
 }
