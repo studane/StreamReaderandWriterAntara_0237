17fea319421fcefae6c3ace0f3a9896a9fb91101
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-18 10:31:24 +0700

    membuat kondisi jika throw bertipe data integer dan bukan integer

--- manualException.cpp
+++ manualException.cpp
@@ -10,4 +10,15 @@ int main()
         throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
-  }
+    //kondisi jika throw yang bertipe data integer
+    catch(int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    //kondisi jika throw bertipe data selain integer
+    catch (...)
+    {
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
